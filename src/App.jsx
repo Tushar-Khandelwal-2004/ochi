2f4ea7e8bffe4df7b12f6de3469e5806ea4a5b38
Tushar-Khandelwal-2004 <khandelwal.tushar8527@gmail.com>
2025-01-19 00:27:34 +0530

    Created Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
+import Navbar from "./components/Navbar"
 
 
 function App() {
   
 
   return (
-    <div className="w-full h-screen bg-zinc-900">
-       the Vite and React logos to learn more
+    <div className="w-full h-screen bg-zinc-900 text-white">
+      <Navbar/>
     </div>
   )
 }
