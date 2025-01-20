e3993ea28bcef98205220ce41bb54c6a6e8a28c0
Tushar-Khandelwal-2004 <khandelwal.tushar8527@gmail.com>
2025-01-20 21:30:08 +0530

    Implemented motion on Landing Page with Ochi

--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -1,5 +1,6 @@
 import { motion } from "framer-motion";
 import { FaArrowUpLong } from "react-icons/fa6";
+import ochi from "./assets/ochi.png"
 function LandingPage() {
     return (
         <div className="w-full h-screen bg-[#F1F1F1] pt-1">
@@ -7,7 +8,7 @@ function LandingPage() {
                 {["We Create", "Eye Opening", "Presentations"].map((item, index) => {
                     return <div className="masker ">
                         <div className="w-fit flex items-end ">
-                            {index===1 && (<div className="mr-[1vw] w-[8vw] rounded-md h-[5.7vw] top-[0.9vw] relative bg-green-500"></div>)}
+                            {index===1 && (<motion.div initial={{width:0,}} animate={{width:"8vw"}} transition={{ease:[0.76, 0, 0.24, 1],duration:1}} className="mr-[1vw] w-[8vw] rounded-md h-[5.7vw] top-[0.9vw] relative overflow-hidden"><img className="w-full h-full" src={ochi} alt="ochi" /></motion.div>)}
                         <h1 className="pt-[2vw] -mb-[1vw] uppercase text-[9vw] leading-[.75] font-custom ">{item}</h1>
 
                         </div>
